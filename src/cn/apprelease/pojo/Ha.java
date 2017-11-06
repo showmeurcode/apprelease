bbc9a346cfad8d6efcf2a85b50690cb738c72bc0
kong-kxz <1172068873@qq.com>
2017-11-06 16:46:44 +0800

    fdef

--- src/cn/apprelease/pojo/Ha.java
+++ src/cn/apprelease/pojo/Ha.java
@@ -5,7 +5,7 @@ public class Ha {
 
     private  String length;
 
-/*wshishi ewe*/
+/*wshishi ewe我是孔*/
 
     	/*INSERT  INTO  smbms_user(userCode, userName, userPassword, gender, birthday, phone, address, userRole, createdBy, creationDate, modifyBy, modifyDate)
 		VALUES (#{userCode},#{userName},#{userPassword},#{gender},#{birthday},#{phone},#{address},#{userRole},#{createdBy},#{creationDate},#{modifyBy},#{modifyDate})
