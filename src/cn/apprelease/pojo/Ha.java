763f5a110c1247e8f43c168087c273dd1a1600b5
xiaochenxinqing <842946343@qq.com>
2017-11-06 16:15:00 +0800

    修改ha

--- src/cn/apprelease/pojo/Ha.java
+++ src/cn/apprelease/pojo/Ha.java
@@ -5,4 +5,10 @@ public class Ha {
 
     private  String length;
 
+
+
+    	/*INSERT  INTO  smbms_user(userCode, userName, userPassword, gender, birthday, phone, address, userRole, createdBy, creationDate, modifyBy, modifyDate)
+		VALUES (#{userCode},#{userName},#{userPassword},#{gender},#{birthday},#{phone},#{address},#{userRole},#{createdBy},#{creationDate},#{modifyBy},#{modifyDate})
+*/
+
 }
