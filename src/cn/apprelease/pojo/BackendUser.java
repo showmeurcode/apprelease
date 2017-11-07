e5dc1e292d6f6126fa8c750b5dab918640de0fe8
kong-kxz <1172068873@qq.com>
2017-11-07 09:16:44 +0800

    xiuzheng

---
+++ src/cn/apprelease/pojo/BackendUser.java
@@ -0,0 +1,97 @@
+package cn.apprelease.pojo;
+
+import java.util.Date;
+
+/**
+ * @author kongxiangzhong
+ * 后台用户
+ * 2017/11/7 8:32
+ */
+public class BackendUser {
+
+    private int id;//主键id
+    private String userCode; //用户编码
+    private String userName; //用户名称
+    private String userType; //用户角色类型
+    private int createdBy; //创建者
+    private Date creationDate; //创建时间
+    private int modifyBy; //更新者
+    private Date modifyDate; //更新时间
+    private  String userPassword; //用户密码
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(int modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+
+
+}
+
