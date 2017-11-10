38f2a8e162bd6f46a63020281c356372e6c0629d
zwYDai <912119821@qq.com>
2017-11-10 10:34:20 +0800

    .

--- src/cn/apprelease/controller/AppInfoController.java
+++ src/cn/apprelease/controller/AppInfoController.java
@@ -1,10 +1,13 @@
 package cn.apprelease.controller;
 
+import cn.apprelease.pojo.AppInfo;
 import cn.apprelease.service.app_info.AppInfoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author zwy
@@ -15,6 +18,8 @@ import javax.annotation.Resource;
 public class AppInfoController {
     @Resource
     AppInfoService appInfoService;
+
+
 
 
 }
